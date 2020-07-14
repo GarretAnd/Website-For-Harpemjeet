2f8c73a5d6b8ff6e74e5083067d22fb3a4550d25
GarretAnd <Garret.D.Andreine.22@dartmouth.edu>
2020-07-14 16:11:39 -0400

    Optimized Images

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,14 @@ module.exports = {
         ],
       },
       {
+        test: /\.(jpg|png|gif|svg)$/,
+        loader: 'image-webpack-loader',
+        // Specify enforce: 'pre' to apply the loader
+        // before url-loader/svg-url-loader
+        // and not duplicate it in rules with them
+        enforce: 'pre',
+      },
+      {
         test: /\.(jpe?g|png|gif|svg)$/,
         use: [
           {
