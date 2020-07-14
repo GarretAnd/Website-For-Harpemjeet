4b4382417a9b2c236fb3c67b7eae8376932b0b84
GarretAnd <Garret.D.Andreine.22@dartmouth.edu>
2020-07-14 15:25:27 -0400

    dropped rule

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,14 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'babel-loader' },
+          { loader: 'eslint-loader' },
+        ],
+      },
+      {
         test: /\.(jpe?g|png|gif|svg)$/,
         use: [
           {
